aea8ac557db403cef53b4874c923510e5cfe49a8
victor souza <victor.ti.souza@gmail.com>
2021-09-13 15:10:00 -0300

    feat: SalvarContato no controller finalizada

--- controllers/contatoController.go
+++ controllers/contatoController.go
@@ -1,9 +1,26 @@
 package controllers
 
 import (
+	"agenda/modules/models"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func SalvarContato(gc *gin.Context) {
+	var novoContato models.Contato
+
+	if gc.BindJSON(&novoContato) != nil {
+		panic("Ocorreu um problema com o bind do Objeto Json")
+	}
+
+	lastId, err := models.SalvarContato(novoContato.Nome, novoContato.Email, novoContato.Telefone, novoContato.Celular, novoContato.Endereco)
+
+	if err == nil {
+		gc.JSON(http.StatusInternalServerError, gin.H{"status": "Houve um erro ao processar sua requisição"})
+	} else {
+		novoContato.Id = lastId
+		gc.IndentedJSON(http.StatusCreated, novoContato)
+	}
 
 }
