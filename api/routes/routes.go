f2a882de2d6726065939d3e7170f460470611ae4
victor souza <victor.ti.souza@gmail.com>
2021-09-19 16:20:11 -0300

    refact: Organizando posicao das rotas

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -8,9 +8,9 @@ import (
 
 func CarregaRotas(router *gin.Engine) {
 	router.POST("/contatos", controllers.SalvarContato)
-	router.GET("/contatos", controllers.ListarContatos)
-	router.DELETE("/contatos/:id", controllers.DeletarContato)
-	router.GET("/contatos/:id", controllers.BuscarContato)
 	router.PUT("/contatos/:id", controllers.EditarContato)
 	router.PATCH("/contatos", controllers.EditarParcialContato)
+	router.DELETE("/contatos/:id", controllers.DeletarContato)
+	router.GET("/contatos", controllers.ListarContatos)
+	router.GET("/contatos/:id", controllers.BuscarContato)
 }
